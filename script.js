c02eaae795eb933f99ba3af9a4ccbf76019a5909
sirtawther <104883031+sirtawther@users.noreply.github.com>
2023-07-27 11:17:41 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const button = document.querySelector("button")
+ const input = document.querySelector(".numberInput");
+        const para = document.querySelector("p");
 
-function sumTwoNumber() {
-    const num1 = prompt("Type num1: ")
-    const num2 = prompt("Type num2: ")
-    const output = Number(num1)+Number(num2)
-    document.getElementById("info").innerHTML = `You are adding ${num1} with ${num2}.`
-    document.getElementById("header").innerHTML = `Output result is ${output}.`
-}
+        function doubleNum(num) {
+            return num * num
+        }
 
-addEventListener("click",sumTwoNumber)
+        input.addEventListener("change", ()=> {
+            const num = parseFloat(input.value)
+            para.textContent = doubleNum(num)
+        })
