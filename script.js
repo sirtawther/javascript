0c76ed3638799a3f5de2bc3237e48d4901199225
sirtawther <104883031+sirtawther@users.noreply.github.com>
2023-07-28 10:32:25 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-const button = document.querySelector("button")
+function askQuestion(question, male, female) {
 
-function sumTwoNumber() {
-    const num1 = prompt("Type num1: ")
-    const num2 = prompt("Type num2: ")
-    const output = Number(num1)+Number(num2)
-    document.getElementById("info").innerHTML = `You are adding ${num1} with ${num2}.`
-    document.getElementById("header").innerHTML = `Output result is ${output}.`
+    if (question == "Male") male()
+    else female()
 }
 
-addEventListener("click",sumTwoNumber)
+
+askQuestion("Male", function () { alert("Mingalar par khinbya") }, function () { alert("Mingalar par shin") })
