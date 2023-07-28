d136be5d2c99e5a4b56e594da5e0bdd03f3a1930
sirtawther <104883031+sirtawther@users.noreply.github.com>
2023-07-28 14:44:26 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-const button = document.querySelector("button")
-
-function sumTwoNumber() {
-    const num1 = prompt("Type num1: ")
-    const num2 = prompt("Type num2: ")
-    const output = Number(num1)+Number(num2)
-    document.getElementById("info").innerHTML = `You are adding ${num1} with ${num2}.`
-    document.getElementById("header").innerHTML = `Output result is ${output}.`
+let multiply = (s) => {
+    return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase()
 }
 
-addEventListener("click",sumTwoNumber)
+alert(multiply("TOUPPER"))
+
