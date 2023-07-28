0e0f3d47e9df66749faef3050cc27700f014db13
sirtawther <104883031+sirtawther@users.noreply.github.com>
2023-07-28 16:55:32 +0630

    controls for Nan

--- script.js
+++ script.js
@@ -4,27 +4,37 @@ let button = document.querySelector("button")
 
 let output = document.querySelector("#output")
 
-button.addEventListener("click",runProgram)
+button.addEventListener("click", runProgram)
 function runProgram() {
     let number = parseInt(input.value)
     let result = ""
- 
-    alert(isNaN(number))
-    for(let i=1; i<=number; i++){
-        if (i % 3 == 0 && i % 5 ==0) {
-            result += "FizzBuzz" + "<br>"
-        } else if (i % 3 == 0) {
-            result += "Fizz" + "<br>"
-        }else if(i % 5 == 0) {
-            result += "Buzz" + "<br>"
+    output.innerHTML = ""
+
+    if (isNaN(number)) {
+
+        alert("Type a number")
+
+    } else {
+        for (let i = 1; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result += "FizzBuzz" + "<br>"
+            } else if (i % 3 == 0) {
+                result += "Fizz" + "<br>"
+            } else if (i % 5 == 0) {
+                result += "Buzz" + "<br>"
+            }
+            else {
+                result += i + "<br>"
+            }
         }
-        else {
-            result += i + "<br>"
-        }
+
+        output.innerHTML = result
+
     }
-    
-    output.innerHTML = result
-   
+
+
+
+
 
 
 }
