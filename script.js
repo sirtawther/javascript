f46002f46b9f1dd5b94b9074017317a2f4f088ea
sirtawther <104883031+sirtawther@users.noreply.github.com>
2023-07-28 16:40:03 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-const button = document.querySelector("button")
+let input = document.querySelector("input")
 
-function sumTwoNumber() {
-    const num1 = prompt("Type num1: ")
-    const num2 = prompt("Type num2: ")
-    const output = Number(num1)+Number(num2)
-    document.getElementById("info").innerHTML = `You are adding ${num1} with ${num2}.`
-    document.getElementById("header").innerHTML = `Output result is ${output}.`
+let button = document.querySelector("button")
+
+let output = document.querySelector("#output")
+
+button.addEventListener("click",runProgram)
+function runProgram() {
+    let number = parseInt(input.value)
+    let result = ""
+ 
+    alert(isNaN(number))
+    for(let i=1; i<=number; i++){
+        if (i % 3 == 0 && i % 5 ==0) {
+            result += "FizzBuzz" + "<br>"
+        } else if (i % 3 == 0) {
+            result += "Fizz" + "<br>"
+        }else if(i % 5 == 0) {
+            result += "Buzz" + "<br>"
+        }
+        else {
+            result += i + "<br>"
+        }
+    }
+    
+    output.innerHTML = result
+   
+
+
 }
-
-addEventListener("click",sumTwoNumber)
