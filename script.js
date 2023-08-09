d204c2207ca74ae702de766f117e9e373853353e
sirtawther <104883031+sirtawther@users.noreply.github.com>
2023-08-09 11:52:31 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
-const button = document.querySelector("button")
+const contacts = [
+    "Chris:2232322",
+    "Sarah:3453456",
+    "Bill:7654322",
+    "Mary:9998769",
+    "Dianne:9384975",
+];
+let button = document.querySelector("button")
 
-function sumTwoNumber() {
-    const num1 = prompt("Type num1: ")
-    const num2 = prompt("Type num2: ")
-    const output = Number(num1)+Number(num2)
-    document.getElementById("info").innerHTML = `You are adding ${num1} with ${num2}.`
-    document.getElementById("header").innerHTML = `Output result is ${output}.`
+let result = document.getElementById("result")
+
+let names = []
+for (const contact in contacts) {
+    let contact_split = contacts[contact].split(":")
+    names.push(contact_split[0])
+
 }
 
-addEventListener("click",sumTwoNumber)
+let contact_list = document.getElementById("contact_list")
+
+contact_list.textContent = names
+
+
+
+button.addEventListener("click", () => {
+    let input_value = document.querySelector("input").value;
+
+    for (let i = 0; i < contacts.length; i++) {
+        let contact_split = contacts[i].split(":")
+
+
+
+
+        if (contact_split[0].toLowerCase() == input_value.toLowerCase()) {
+            result.textContent = `${contact_split[0]}'s phone number is ${contact_split[1]}.`
+            break
+        } else if (input_value == "") {
+            result.textContent = `Type something!`
+        } else {
+            result.textContent = `Not Found!`
+        }
+
+
+
+    }
+}
+
+)
+
