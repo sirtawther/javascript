d68613c513166447cc7703d9792eaeb4ac29dc3b
sirtawther <104883031+sirtawther@users.noreply.github.com>
2023-08-09 15:35:25 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const button = document.querySelector("button")
+let user_input = +prompt("Type a number for countdown");
+let output = document.querySelector(".output");
 
-function sumTwoNumber() {
-    const num1 = prompt("Type num1: ")
-    const num2 = prompt("Type num2: ")
-    const output = Number(num1)+Number(num2)
-    document.getElementById("info").innerHTML = `You are adding ${num1} with ${num2}.`
-    document.getElementById("header").innerHTML = `Output result is ${output}.`
+let i = 0;
+if (user_input == 0) {
+    output.textContent = "Blast off"
+} else if (user_input > 10) {
+    output.textContent = user_input
+} else {
+
+    
+    while (i < user_input) {
+        let para = document.createElement("p")
+        para.textContent = user_input
+        output.appendChild(para)
+        
+        user_input--;
+    }
+
+    document.getElementById("blastoff").textContent = "Blast off"
+    
 }
-
-addEventListener("click",sumTwoNumber)
